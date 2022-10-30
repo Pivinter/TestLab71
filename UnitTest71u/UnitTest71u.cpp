3e2f0fb8164de1c01ac2e63ab9db5f743b34359b
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-10-30 22:44:39 +0200

    Add project files.

---
+++ UnitTest71u/UnitTest71u.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71u
+{
+	TEST_CLASS(UnitTest71u)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
